5c80628034ce2cc86343b864ade86bccb71f3e50
LissaOrey <azatgoar@mail.ru>
2020-07-03 00:54:36 +0300

    это 1-20 уроки по реакту

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return <header className={s.header}>
+        {/* <img src='https://img2.freepng.ru/20180629/cft/kisspng-logo-business-graphic-designer-glory-5b368ae87b96a3.4372358915303011605062.jpg' /> */}
+        <img src='https://freski.shop/uploads/mural/jpg/cd159195-42be-49fe-9558-9049ac540cf6.jpg' />
+    </header>
+}
+export default Header;
---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Post.module.css';
+
+const Post = (props) => {
+    return (
+        <div className={s.item}>
+            <img src="https://i.pinimg.com/originals/02/c1/ab/02c1abf429d7e4ca96a4c96eb636ed97.jpg" alt="" />
+            {props.message}
+            <div>
+                <span>like:</span> { props.likesCount }
+            </div>
+        </div>
+    )
+}
+export default Post;
