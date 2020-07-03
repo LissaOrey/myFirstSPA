f7def13e398a5fcbebad663e84da3400f1ffda7f
LissaOrey <azatgoar@mail.ru>
2020-07-03 17:17:00 +0300

    21 урок. немного сверстали диалоги

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
-import './Dialogs.module.css';
+import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      {/* <div className={s.ageName}>Dialogs</div> */}
+      <div className={s.dialogItems}>
+        <div className={s.dialog+ ' '+ s.active}>
+          Dymich
+        </div>
+        <div className={s.dialog}>
+          Alena
+        </div>
+        <div className={s.dialog}>
+          Valera
+        </div>
+        <div className={s.dialog}>
+          Layla
+        </div>
+        <div className={s.dialog}>
+          Petr
+        </div>
+        <div className={s.dialog}>
+          Andrey
+        </div>
+        <div className={s.dialog}>
+          Violetta
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>
+          Hi!
+        </div>
+        <div className={s.message}>
+          How are you?
+        </div>
+        <div className={s.message}>
+          I cann't stop to codeing
+        </div>
+      </div>
     </div>
   )
 }
